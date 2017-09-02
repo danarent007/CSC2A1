87ee00a97a3e9b3f95091287ed2658936e43ade1
danarent007 <danielvorster2009@gmail.com>
2017-09-02 10:51:38 +0200

    Updated.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,18 +14,26 @@ import java.util.concurrent.ForkJoinPool;
 public class Main
 {
     public static double[] arr;
-    
+    public static int filterWidth;
+    public static int SEQUENTIAL_CUTOFF = 5;
     
     public static void main(String[] args)
     {
         
-        double[] arr = {1,2,3,4,5,6,7,8,9};
+        //double[] arr = {1,2,3,4,5,6,7,8,9};
+        
+        int filterWidth = 5;
 
         
-        //ForkJoinPool fjpool = new ForkJoinPool();
-        ForkJoinPool.commonPool().invoke(new ForkJoinTask(0,arr.length));
+        ForkJoinPool fjpool = new ForkJoinPool();
+        fjpool.commonPool().invoke(new ForkJoinTask(0,arr.length));
         System.out.println("Done");
         
+    }
+    
+    static final ForkJoinPool fjPool = new ForkJoinPool();
+    void doTask(){
+    fjPool.commonPool().invoke(new ForkJoinTask(0,arr.length));
     }
     
     
